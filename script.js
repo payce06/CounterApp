148cf1a9826a50264fa9460e73f8cc4ac9faaeb9
payce06 <paycegrossman@gmail.com>
2025-04-29 21:01:29 -0400

    counter-app-js

---
+++ script.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+const countDisplay = document.getElementById('count');
+const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
+const resetBtn = document.getElementById('reset');
+
+incrementBtn.addEventListener('click', () => {
+    count++;
+    updateDisplay();
+});
+
+decrementBtn.addEventListener('click', () => {
+    count--;
+    updateDisplay();
+})
